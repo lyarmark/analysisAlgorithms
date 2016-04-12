e07fc77c751e65e4ee87741ff8aef8d41aab96e0
lyarmark <lyarmark@student.touro.edu>
2016-04-12 03:19:32 -0400

    timing regular factoring

--- src/primes/Primes.java
+++ src/primes/Primes.java
@@ -4,17 +4,21 @@ import java.math.BigInteger;
 import java.util.concurrent.TimeUnit;
 
 public class Primes {
+	static final BigInteger TWO = new BigInteger("2");
+	static final BigInteger THREE = new BigInteger("3");
 
 	public static void main(String[] args) {
-		BigInteger p = new BigInteger("31");
-		BigInteger q = new BigInteger("63");
-		// BigInteger p = new BigInteger("48112959837082048697");
-		// BigInteger q = new BigInteger(" 513821217024129243948411056803");
+
+		// BigInteger p = new BigInteger("31");
+		// BigInteger q = new BigInteger("63");
+		BigInteger p = new BigInteger("48112959837082048697");
+		BigInteger q = new BigInteger("513821217024129243948411056803");
 
 		BigInteger n = p.multiply(q);
 		long startTime = System.currentTimeMillis();
 		PollardRho pr = new PollardRho();
-		pr.factor(n);
+		// pr.factor(n);
+		factor(n);
 		long endTime = System.currentTimeMillis();
 		long totalTime = endTime - startTime;
 		String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(totalTime),
@@ -22,5 +26,13 @@ public class Primes {
 				TimeUnit.MILLISECONDS.toSeconds(totalTime) % TimeUnit.MINUTES.toSeconds(1));
 		System.out.println(hms);
 
+	}
+
+	public static void factor(BigInteger n) {
+		for (BigInteger i = TWO; i.compareTo(n) < 0; i = i.add(BigInteger.ONE)) {
+			if (n.mod(i).equals(BigInteger.ZERO)) {
+				System.out.println(i);
+			}
+		}
 	}
 }
